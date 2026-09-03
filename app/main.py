f291d86208ecd922ffee5d774cabc3f97b0993bd
MayEnjoyer <lemurkanikitka@mail.ru>
2026-09-03 12:52:05 +0300

    implement custom exceptions for ServerConnection methods

--- app/main.py
+++ app/main.py
@@ -1,18 +1,32 @@
 from ssh.credentials import ServerCredentials
-from ssh.connection import ServerConnection
+from ssh.connection import ServerConnection, SSHConnectionError, SSHExecutionError
+from getpass import getpass
+
+def get_credentials():
+    print("Input host IP-address:", end=" ")
+    _host = input()
+    print("Input username:", end=" ")
+    _username = input()
+    _password = getpass(prompt="Input password: ", echo_char="*")
+
+    return ServerCredentials(_host, _username, _password)
 
 client = None
 
 try:
-    credentials = ServerCredentials()
+    credentials = get_credentials()
 
     client = ServerConnection(credentials)
 
     client.set_connection()
 
-    output, er = client.execute_command("pwd")
+    output, er = client.execute_command("ll")
 
     print(output)
+except SSHConnectionError as er:
+    print(f"Connection failed: {er}")
+except SSHExecutionError as er:
+    print(f"Command execution failed: {er}")
 finally:
     if client:
         client.close_connection()
--- app/ssh/connection.py
+++ app/ssh/connection.py
@@ -1,6 +1,12 @@
 from ssh.credentials import ServerCredentials
 import paramiko
 
+class SSHConnectionError(Exception):
+    pass
+
+class SSHExecutionError(Exception):
+    pass
+
 class ServerConnection:
     def __init__(self, credentials: ServerCredentials):
         self.credentials = credentials
@@ -11,17 +17,25 @@ class ServerConnection:
         )
 
     def set_connection(self):
-        self.client.connect(
-            hostname=self.credentials.host,
-            username=self.credentials.username,
-            port=self.credentials.port,
-            password=self.credentials.password
-        )
+        try:
+            self.client.connect(
+                hostname=self.credentials.host,
+                username=self.credentials.username,
+                port=self.credentials.port,
+                password=self.credentials.password
+            )
+        except paramiko.SSHException as er:
+            raise SSHConnectionError(er)
+        except OSError as er:
+            raise SSHConnectionError(er.strerror)
 
     def execute_command(self, command_name: str):
-        _in, _out, _er = self.client.exec_command(command_name)
+        try:
+            _in, _out, _er = self.client.exec_command(command_name)
 
-        return (_out.read().decode(), _er.read().decode())
+            return (_out.read().decode(), _er.read().decode())
+        except paramiko.SSHException as er:
+            raise SSHExecutionError(er)
 
     def close_connection(self):
         self.client.close()
