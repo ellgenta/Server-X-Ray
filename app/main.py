b3144925ae5baed6c6d9eb749b1fe64d33c1651e
MayEnjoyer <lemurkanikitka@mail.ru>
2026-09-03 13:34:53 +0300

    add SFTP session handlers

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from ssh.credentials import ServerCredentials
-from ssh.connection import ServerConnection, SSHConnectionError, SSHExecutionError
+from ssh.connection import ServerConnection, SSHConnectionError, SSHExecutionError, SFTPSessionError
 from getpass import getpass
 
 def get_credentials():
@@ -20,13 +20,18 @@ try:
 
     client.set_connection()
 
-    output, er = client.execute_command("ll")
+    client.open_sftp_session()
+
+    output, er = client.execute_command("ls -la")
 
     print(output)
 except SSHConnectionError as er:
     print(f"Connection failed: {er}")
 except SSHExecutionError as er:
     print(f"Command execution failed: {er}")
+except SFTPSessionError as er:
+    print(f"SFTP-session error: {er}")
 finally:
     if client:
+        client.close_sftp_session()
         client.close_connection()
--- app/ssh/connection.py
+++ app/ssh/connection.py
@@ -7,10 +7,14 @@ class SSHConnectionError(Exception):
 class SSHExecutionError(Exception):
     pass
 
+class SFTPSessionError(Exception):
+    pass
+
 class ServerConnection:
     def __init__(self, credentials: ServerCredentials):
         self.credentials = credentials
         self.client = paramiko.SSHClient()
+        self.sftp_server = None
 
         self.client.set_missing_host_key_policy(
             paramiko.AutoAddPolicy()
@@ -39,3 +43,14 @@ class ServerConnection:
 
     def close_connection(self):
         self.client.close()
+
+    def open_sftp_session(self):
+        try:
+            self.sftp_server = self.client.open_sftp()
+        except paramiko.SSHException as er:
+            raise SFTPSessionError(er)
+
+    def close_sftp_session(self):
+        if self.sftp_server:
+            self.sftp_server.close()
+        self.sftp_server = None
