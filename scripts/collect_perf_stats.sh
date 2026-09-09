3d83256dda8854efc4543b678bad8641e6a0ecc7
MayEnjoyer <lemurkanikitka@mail.ru>
2026-09-09 13:08:27 +0300

    update performance collector

--- scripts/collect_perf_stats.sh
+++ scripts/collect_perf_stats.sh
@@ -15,9 +15,9 @@ record_ram_stats() {
     local dst_dir="$1"
 
     if ! free --mega | awk -v dir="$dst_dir" '
-    NR==1 { for (i=2; i<=NF; i++) headers[i-1]=$i }
-    NR==2 { for (i=2; i<=NF; i++) { print headers[i-1] "=" $i > (dir "/ram_stats.txt") } }
-    NR==3 { for (i=2; i<=NF; i++) { print headers[i-1] "=" $i > (dir "/swap_stats.txt") } }
+    NR==1 { for (i=1; i<=NF; i++) headers[i]=$i }
+    NR==2 { for (i=2; i<=NF; i++) {print headers[i-1] "=" $i > (dir "/ram_stats.txt")} }
+    NR==3 { for (i=2; i<=NF; i++) {print headers[i-1] "=" $i > (dir "/swap_stats.txt")} }
     END { if (NR < 3) exit 1 }
     '; then
         echo "Error while collecting or parsing data" >&2
@@ -30,8 +30,35 @@ record_ram_stats() {
 record_cpu_stats() {
     local dst_dir="$1"
 
-    if ! awk -v dir="$dst_dir" '
-    {printf "last_1="$1"\nlast_5="$2"\nlast_15="$3"\n" > (dir "/load_avg.txt")}' /proc/loadavg; then 
+    if ! awk -v path="$dst_dir""/la_stats.txt" '
+    {
+    print "last_1="$1 > path
+    print "last_5="$2 >> path
+    print "last_15="$3 >> path
+    }
+    END {if (NR != 1) exit 1}
+    ' /proc/loadavg; then
+        echo "Error while collecting or parsing data" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+record_disk_stats() {
+    local dst_dir="$1"
+
+    if ! df -B MB | tail -n +2 | awk -v dir="$dst_dir" '
+    {
+    path=dir"/fs_"NR
+    print "fs_name=" $1 > path".txt"
+    print "size=" $2 >> path".txt"
+    print "used=" $3 >> path".txt"
+    print "available=" $4 >> path".txt"
+    print "mount=" $6 >> path".txt"
+    }
+    END {if (NR < 1) exit 1}
+    '; then
         echo "Error while collecting or parsing data" >&2
         return 1
     fi
@@ -46,5 +73,7 @@ mkdir -p "$perf_dir" || exit 1
 record_ram_stats "$perf_dir" || exit 1
 
 record_cpu_stats "$perf_dir" || exit 1
+
+record_disk_stats "$perf_dir" || exit 1
 
 exit 0
